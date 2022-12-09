ef9bd2672a0f89f345dd00d54f49681c5d5b9243
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2022-12-09 12:07:05 +0100

    add components

---
+++ src/components/Phrase.js
@@ -0,0 +1,16 @@
+import styles from './Phrase.module.css'
+
+
+export default function Phrase() {
+  return (
+    <div className={styles.container}>
+      
+      <h1>Speak only when you feel your words are better than the silence</h1>
+      <br />
+      
+      <br />
+      <h1>You learn more from failure than from success. Don’t let it stop you. Failure builds character.</h1>
+            
+    </div>
+  )
+}
